c0f501bc5f6680111f12c8fba1ee6afb65787c35
Burak Özkan <burak.ozkan@path.com.tr>
2021-09-16 23:30:10 +0300

    add model & controller

---
+++ models/productModel.js
@@ -0,0 +1,12 @@
+const products = require("../data/products");
+
+function findAll() {
+  return new Promise((resolve, reject)=> {
+    resolve(products)
+
+  });
+};
+
+module.exports = {
+  findAll
+}
