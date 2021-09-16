c15206ac5628c22e63181e01aeef696b176c0c7b
Burak Özkan <burak.ozkan@path.com.tr>
2021-09-16 23:15:17 +0300

    add simple route

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const http = require("http");
 const products = require("./data/products");
 
 const server = http.createServer((req, res)=>{
-  res.writeHead(200,{"Content-Type": "application/json"});
-  res.end(JSON.stringify(products))
+  if(req.url === '/api/products' && req.method === 'GET'){
+    res.writeHead(200,{"Content-Type": "application/json"});
+    res.end(JSON.stringify(products));
+  } else {
+    res.writeHead(404,{"Content-Type": "application/json"});
+    res.end(JSON.stringify({"message": "Page not found" }));
+  }
 });
 
 PORT = process.env.PORT || 5000
